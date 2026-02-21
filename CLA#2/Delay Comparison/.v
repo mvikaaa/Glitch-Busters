e136a7c806d83a50753741a844651aa4bd8ec0f6
mvikaaa <222024068+mvikaaa@users.noreply.github.com>
2026-02-21 12:58:00 +0530

    Create .v
    delay compariosn

---
+++ CLA#2/Delay Comparison/.v
@@ -0,0 +1,4 @@
+                         Regular CLA                                       Heirarchical CLA
+Gate Fan-in (Inputs)   Up to 33 inputs per gate.                     Maximum 4 to 5 inputs per gate.
+Path to cout           2–4 gate delays (Theoretical).                ~10–12 gate delays (Practical).
+Physical Speed         Extremely Slow (due to high capacitance).     Extremely Fast (optimized for silicon).
